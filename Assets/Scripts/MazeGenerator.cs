2245192599ea591186e94b573d7cb9c630058a30
Benjamin Most <benjaminmost@gmail.com>
2020-06-13 15:09:38 -0400

    Got basic instantiation of maze walls working

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject wallBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 size = wallBlock.GetComponent<BoxCollider2D>().size;
+        print(size.x);
+        print(size.y);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            Instantiate(wallBlock, new Vector3(size.x * i, size.y * i, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
