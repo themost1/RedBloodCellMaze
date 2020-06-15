7cfecc25bd1bc5cb5fb98620ca7ce6df8b5db683
yiwengao <yiwen.gao@columbia.edu>
2020-06-14 20:31:50 -0500

    Add minor style changes

---
+++ Assets/Scripts/GotoScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToTutorial1()
+    {
+        SceneManager.LoadScene("TutorialScene1");
+    }
+
+    public void GoToTutorial2() 
+    {
+        SceneManager.LoadScene("TutorialScene2");
+    }
+
+    public void GoToMazeScene()
+    {
+        PlayerController.hp = 3;
+        PlayerController.shieldsLeft = 0;
+        SceneManager.LoadScene("MazeScene");
+    }
+}
